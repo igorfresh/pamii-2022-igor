793eadeab425389741edaf10c9e6644425f12abd
igorfresh <igormiguel37@gmail.com>
2022-02-21 22:33:55 -0300

    Aula 01

---
+++ olamundo.js
@@ -0,0 +1,18 @@
+console.log('Olá mundo')
+var a = 2;
+console.log('Esse é o conteúdo de a = ' ,a)
+var b = 3;
+console.log('Esse é resultado de ', a , '+', b , '=', a+b)
+console.log('E esse é resultado de ', a , '-', b , '=', a-b)
+console.log('Mas o resultado de ', a , "*" , b , '=' , a*b)
+console.log('Entretanto, o resultado de ', a ,'/', b , '=', a/b)
+let nome = 'Igor'
+var idade = 20
+let emprego = 'auxiliar administrativo'
+let empresa = 'Santander'
+let tempo = '6 horas'
+
+console.log('Meu nome é' , nome , ', tenho' , idade , 'anos, trabalho na empresa' , empresa , 'como' , emprego , 'durante' , tempo)
+
+
+// assim que se coloca um comentário = barra barra
